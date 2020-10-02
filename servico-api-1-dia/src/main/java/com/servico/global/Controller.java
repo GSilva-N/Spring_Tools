a20e8403f210d6477bf687276841a56f2bf21989
Guilherme <guilherme.paganini10@gmail.com>
2020-10-02 17:24:47 -0300

    STS

--- servico-api-1-dia/src/main/java/com/servico/global/Controller.java
+++ servico-api-1-dia/src/main/java/com/servico/global/Controller.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,17 @@ public class Controller {
 	public List<ServicoModel> listaServico(){
 		
 		return repository.findAll();
+	}
+	
+	@GetMapping("/servicos/nome/{nome}")
+	public List<ServicoModel> buscarPorNome(@PathVariable String nome){
+		return repository.findByNome(nome);
+	}
+	
+	@PutMapping("/servicos/{id}")
+	public ServicoModel atualizar(@PathVariable Long id, @RequestBody ServicoModel model) {
+		model.setId(id);
+		return repository.save(model);
 	}
 	
 	@PostMapping("/servicos")
